a750ae65f43fe3171922a7adea81a0a781fce855
mohamed_sakr <sakr83117@gmail.com>
2024-02-27 17:52:46 +0200

    Timer + Popup Finished

--- main.js
+++ main.js
@@ -6,12 +6,13 @@ document.querySelector(".start").addEventListener("click", () => {
   } else {
     document.querySelector(".name span").textContent = name;
   }
-
+  startTimer(60);
   document.querySelector(".start").remove();
 });
 
 // variables
 let duration = 1000;
+let matched = 0;
 let blocksContainer = document.querySelector(".block-grid");
 let cards = Array.from(blocksContainer.children);
 let orderRange = [...Array(cards.length).keys()];
@@ -64,6 +65,7 @@ function checkMatch(firstCard, secondCard) {
 
     firstCard.classList.add("matched");
     secondCard.classList.add("matched");
+    matched += 2;
 
     document.getElementById("successSound").play();
   } else {
@@ -73,4 +75,59 @@ function checkMatch(firstCard, secondCard) {
     }, duration);
     document.getElementById("failSound").play();
   }
+}
+
+// time to solve
+
+function startTimer(duration) {
+  const display = document.querySelector(".time span");
+
+  let timer = duration;
+  const interval = setInterval(() => {
+    const minutes = Math.floor(timer / 60)
+      .toString()
+      .padStart(2, "0");
+    const seconds = (timer % 60).toString().padStart(2, "0");
+
+    display.textContent = `${minutes}:${seconds}`;
+    if (matched === cards.length) {
+      clearInterval(interval);
+      showPopup("Congratulations!", "assets/images/sucess.jpg");
+      stopClickingEvent();
+      document.getElementById("winSound").play();
+    }
+    if (--timer < 0) {
+      clearInterval(interval);
+      display.textContent = "00:00";
+      showPopup("Time Over!", "assets/images/TimeOver.jpg");
+      stopClickingEvent();
+      document.getElementById("loseSound").play();
+    }
+  }, 1000);
+}
+
+// Play Again button
+document.querySelector(".popup button").addEventListener("click", () => {
+  cards.forEach((card) => {
+    card.classList.remove("flipped");
+    card.classList.remove("matched");
+    matched = 0;
+    shuffledOrder = shuffle(orderRange);
+    cards.forEach((card, index) => {
+      card.style.order = shuffledOrder[index];
+    });
+    startTimer(60);
+  });
+  hidePopup();
+});
+
+function showPopup(title, img) {
+  const popup = document.querySelector(".popup");
+  popup.querySelector(".popup-title").innerHTML = title;
+  popup.querySelector(".popup-img").src = img;
+  popup.style.display = "flex";
+}
+
+function hidePopup() {
+  document.querySelector(".popup").style.display = "none";
 }
